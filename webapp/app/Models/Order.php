f490da26f91473d666732630455094abc21d1970
Luís Ferreira <contact@lsferreira.net>
2020-11-23 19:15:58 +0000

    webapp: models: add database models
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ webapp/app/Models/Order.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Traits\Enums;
+
+class Order extends Model
+{
+    use HasFactory, Enums;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'notes',
+        'total_price',
+        'date'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'opened_at' => 'datetime',
+        'closed_at' => 'datetime',
+        'current_status_at' => 'datetime'
+    ];
+
+    /**
+     * The type attribute enumeration
+     */
+    protected $enumStatuses = [
+        'H', // Holding
+        'P', // Preparing
+        'R', // Ready
+        'T', // In Transit
+        'D', // Delivered
+        'C'  // Cancelled
+    ];
+}
---
+++ webapp/app/Models/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Traits\Enums;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes, Enums;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'photo_url',
+        'price'
+    ];
+
+    /**
+     * The timestamps are created on insert or update
+     *
+     * @var boolean
+     */
+    public $timestamps = true;
+
+    /**
+     * The row is soft deletable
+     *
+     * @var boolean
+     */
+    protected $softDelete = true;
+
+    /**
+     * The type attribute enumeration
+     */
+    protected $enumTypes = [
+        'hot dish',
+        'cold dish',
+        'drink',
+        'dessert'
+    ];
+}
