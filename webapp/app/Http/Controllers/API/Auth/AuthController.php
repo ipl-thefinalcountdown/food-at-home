a4dab21b6a51a0bfef38a0353a4da5fafcfe4f11
Luís Ferreira <contact@lsferreira.net>
2020-12-05 23:01:07 +0000

    webapp: api: improve user story 3 and implement user story 4 (registration)
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- webapp/app/Http/Controllers/API/Auth/AuthController.php
+++ webapp/app/Http/Controllers/API/Auth/AuthController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -33,18 +37,65 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        if($request->boolean('all'))
-        {
-            $request->user()->tokens()->delete();
-        }
-        else
-        {
-            $request->user()->currentAccessToken()->delete();
-        }
+        $user = $request->user();
+
+        $tokens = ($request->boolean('all'))
+            ? $user->tokens()
+            : $user->currentAccessToken();
+
+        $tokens->delete();
 
         return response()->json([
             'status_code' => 200,
             'message' => 'Logout successfully.'
         ]);
+    }
+
+    public function register(Request $request)
+    {
+        if(Auth::check())
+        {
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized register on authenticated user.'
+            ], 401);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|regex:/^[A-Za-záàâãéèêíóôõúçÁÀÂÃÉÈÍÓÔÕÚÇ ]+$/',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:3',
+            'address' => 'required',
+            'phone' => 'required',
+            'nif' => 'nullable|numeric|digits:9',
+            'photo' => 'nullable|image|max:8192'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'status_code' => 400,
+                'message' => 'Bad request. Invalid data.',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $user = new User();
+        $user->fill($request->only('name', 'email', 'password'));
+        $user->password = Hash::make($user->password);
+        $user->save();
+
+        $customer = new Customer();
+        $customer->id = $user->id;
+        $customer->fill($request->only('address', 'phone', 'nif'));
+        $customer->user()->associate($user);
+
+        if ($request->hasFile('photo')) {
+            $path = $request->photo->store('public/fotos');
+            $customer->photo_url = basename($path);
+        }
+
+        $customer->push();
+
+        return response()->json($customer, 201);
     }
 }
--- webapp/routes/api.php
+++ webapp/routes/api.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', 'Auth\AuthController@login')->name('login');
 Route::get('/products', 'ProductController@index')->name('products');
 Route::get('/products/{product}', 'ProductController@view')->name('product');
+
+Route::middleware('guest')->group(function() {
+    Route::post('/login', 'Auth\AuthController@login')->name('login');
+    Route::post('/register', 'Auth\AuthController@register')->name('register');
+});
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', 'UserController@index');
