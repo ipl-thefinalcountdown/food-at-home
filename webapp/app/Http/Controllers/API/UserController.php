0566580d3ce311fd8b6c888e350fca5e17ec4aaf
Luís Ferreira <contact@lsferreira.net>
2020-12-23 16:37:50 +0000

    webapp: api: improve auth controllers
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- webapp/app/Http/Controllers/API/UserController.php
+++ webapp/app/Http/Controllers/API/UserController.php
@@ -6,11 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Customer;
 
 class UserController extends Controller
 {
 	public function index(Request $request)
 	{
-		return $request->user();
+		$cur_user = $request->user();
+
+		if($cur_user->type == 'C')
+			return Customer::where('customers.id', $cur_user->id)
+				->join('users', 'customers.id', '=', 'users.id')
+				->select('*')
+				->first();
+		else
+			return $cur_user;
 	}
 }
