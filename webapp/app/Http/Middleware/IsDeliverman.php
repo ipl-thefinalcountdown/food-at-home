5cbf842cf5a86612058d0def50ea838a55f1ad3f
Luís Ferreira <contact@lsferreira.net>
2020-12-26 00:41:02 +0000

    webapp: add server-side user middlewares
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ webapp/app/Http/Middleware/IsDeliverman.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class IsDeliverman
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() && $request->user()->type == 'ED')
+        {
+            return $next($request);
+        }
+
+        throw new AccessDeniedHttpException('Unauthorized.');
+    }
+}
