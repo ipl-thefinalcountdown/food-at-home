0b807a2f6ded2145f0ee45807c953178786f8bed
Luís Ferreira <contact@lsferreira.net>
2020-12-26 00:43:01 +0000

    webapp: add user validator helper
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ webapp/app/Helpers/UserValidator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+
+class UserValidator
+{
+	public static function validateOnCreate(Request $request)
+	{
+        $validator_assocArr = [
+            'name' => 'required|regex:/^[A-Za-záàâãéèêíóôõúçÁÀÂÃÉÈÍÓÔÕÚÇ ]+$/',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:3',
+            'photo' => 'nullable|image|max:8192'
+        ];
+
+        if($request->has('type') && $request->type == 'C')
+        {
+            $validator_assocArr += [
+                'address' => 'required',
+                'phone' => 'required',
+                'nif' => 'nullable|numeric|digits:9'
+            ];
+        }
+
+		return Validator::make($request->all(), $validator_assocArr);
+    }
+
+    public static function validateOnUpdate(Request $request, User $user)
+	{
+        $validator_assocArr = [
+            'name' => 'required|regex:/^[A-Za-záàâãéèêíóôõúçÁÀÂÃÉÈÍÓÔÕÚÇ ]+$/',
+            'email' => ['required', 'string', 'email', 'max:255',
+                Rule::unique('users', 'email')->using(function ($query) use ($user) {
+                    $query->where('id', '<>', $user->id);
+                })
+            ],
+            'photo' => 'nullable|image|max:8192'
+        ];
+
+        if($user->type == 'C')
+        {
+            $validator_assocArr += [
+                'address' => 'required',
+                'phone' => 'required',
+                'nif' => 'nullable|numeric|digits:9'
+            ];
+        }
+
+		return Validator::make($request->all(), $validator_assocArr);
+	}
+}
