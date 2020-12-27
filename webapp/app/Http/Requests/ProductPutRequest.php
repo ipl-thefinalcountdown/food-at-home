a82ab8113cc2572f77aa0b72935832731d23bfb4
João Lourenço <jlourenco5691@gmail.com>
2020-12-27 01:54:57 +0000

    webapp: app: requests: add custom product post and put requests
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ webapp/app/Http/Requests/ProductPutRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductPutRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // FIXME: only a manager can access this
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|regex:/^[A-Za-záàâãéèêíóôõúçÁÀÂÃÉÈÍÓÔÕÚÇ ]+$/',
+            'price' => 'required|numeric|max:999999.99|min:0.00',
+            'type' => 'in:drink,dessert,hot dish,cold dish',
+            'description' => 'required',
+            'photo_url' => 'image|max:8192'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'A name is required',
+            'name.regex' => 'Name contains invalid characters',
+            'price.required' => 'Price is required',
+            'price.numeric' => 'Price must be a number',
+            'price.max' => 'Price must be bellow 999999.99',
+            'price.min' => 'Price can not be negative',
+            'type.in' => 'Product type must be one of (drink, dessert, hot dish, cold dish)',
+            'description.required' => 'Description is required, please insert an empty text for no description',
+            'photo_url.image' => 'Photo must be an image',
+            'photo_url.max' => 'Maximum photo size is 8192'
+        ];
+    }
+}
