f2f647967a5a6885547ea2672541ce9794ed49f3
João Lourenço <jlourenco5691@gmail.com>
2021-01-04 19:32:07 +0000

    webapp: app: userController: fix user's update methods authorization and validation
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- webapp/app/Http/Requests/UserPutRequest.php
+++ webapp/app/Http/Requests/UserPutRequest.php
@@ -17,11 +17,6 @@ class UserPutRequest extends FormRequest
         return $this->user->type != 'C';
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
@@ -34,19 +29,6 @@ class UserPutRequest extends FormRequest
             'password' => 'nullable|min:3',
             'photo' => 'nullable|image|max:8192',
             'type' => 'required|in:EC,ED,EM',
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-            'name.regex' => 'Invalid name format',
-            'email.email' => 'Invalid email',
-            'email.unique' => 'A registed user already has the same email',
-            'password.min' => 'Password must have a minimum of 3 characters',
-            'photo.image' => 'Photo must be an image',
-            'photo.max' => 'Photo must not be higher than 8192',
-            'type.in' => 'Type must be one of (EC, ED, EM)',
         ];
     }
 }
