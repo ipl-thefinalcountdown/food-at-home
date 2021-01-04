b141eb466733b2a9ad9a9b6d1e21881280d6fbd3
João Lourenço <jlourenco5691@gmail.com>
2021-01-04 17:31:01 +0000

    webapp: app: userController: fix user delete authorization
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ webapp/app/Http/Requests/UserDeleteRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserDeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user != 'C'
+            && !($this->user()->type == 'EM' && $this->user()->id == $this->user->id) ;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
